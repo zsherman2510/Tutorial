bb911ead77dbc0dcb53965583b82c72e95873bc1
zavion sherman <zsherman2510@hotmail.com>
2021-07-19 19:14:12 -0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,91 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect } from "react";
+import "./App.css";
 
 function App() {
+  const [firstName, setFirstName] = useState("");
+  const [lastName, setLastName] = useState("");
+  const [age, setAge] = useState(0);
+  const [usersList, setUsersList] = useState([]);
+
+  const addUser = async () => {
+    const data = {
+      firstName: firstName,
+      lastName: lastName,
+      age: age,
+    };
+
+    const settings = {
+      method: "POST",
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(data),
+    };
+
+    try {
+      const res = await fetch("http://localhost:3001/users", settings);
+      if (res.ok) {
+        return res.json();
+      }
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  const getUsers = async () => {
+    try {
+      const res = await fetch("http://localhost:3001/users");
+      const data = await res.json();
+      if (res.ok) {
+        setUsersList(data);
+      }
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  useEffect(() => {
+    getUsers();
+  }, [usersList]);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div>
+        <label htmlFor="">First Name:</label>
+        <input
+          type="text"
+          onChange={(e) => {
+            setFirstName(e.target.value);
+          }}
+        />
+        <label htmlFor="">Last Name:</label>
+        <input
+          type="text"
+          onChange={(e) => {
+            setLastName(e.target.value);
+          }}
+        />
+        <label htmlFor="">Age:</label>
+        <input
+          type="number"
+          onChange={(e) => {
+            setAge(e.target.value);
+          }}
+        />
+      </div>
+
+      <button onClick={addUser}>Submit</button>
+
+      <div className="usersList">
+        {usersList.map((user) => (
+          <div key={user.id}>
+            <h3>{user.firstName}</h3>
+            <h2>{user.lastName}</h2>
+            <h5>{user.age}</h5>
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
